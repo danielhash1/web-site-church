59c5c544f4872c79b1aa2a81e22a19306d891830
Jiregna Terfa <jirenlife@gmail.com>
2024-06-25 16:44:01 +0200

    content controller and navbar created

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
  skip_before_action :authenticate_user!
   def index
+
   end
 
   def about
+  end
+
+  def home
+   @contents = Content.all
   end
 end
